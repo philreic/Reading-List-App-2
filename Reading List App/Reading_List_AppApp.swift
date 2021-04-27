3ea68a1ed50e4c6e190e1e4235ad57c8512e37f5
bowenzhu1 <bowenzhus@hotmail.com>
2021-04-26 23:21:13 -0400

    Initial Commit

---
+++ Reading List App/Reading_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reading_List_AppApp.swift
+//  Reading List App
+//
+//  Created by CodeWithChris on 2021-04-26.
+//
+
+import SwiftUI
+
+@main
+struct Reading_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
